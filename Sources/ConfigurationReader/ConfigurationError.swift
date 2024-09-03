0f570d369dac1568a5bd2f8276551aa2fa25d196
Manolo van Ee <manolo.vanee@gmail.com>
2024-09-03 12:16:44 +0200

    Add description for ConfigurationError

--- Sources/ConfigurationReader/ConfigurationError.swift
+++ Sources/ConfigurationReader/ConfigurationError.swift
@@ -5,3 +5,24 @@ public enum ConfigurationError: Error {
     case configurationDataInitializationError(type: any ConfigurationData.Type, error: any Error)
     case unknownError(any Error)
 }
+
+extension ConfigurationError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .fileTooLarge(let fileName):
+            "file too large: \(fileName)"
+
+        case .missingFile(let fileName):
+            "missing file: \(fileName)"
+
+        case .fileReadError(let fileName, let error):
+            "failed to read from \(fileName): \(error)"
+
+        case .configurationDataInitializationError(let type, let error):
+            "failed to initialize \(type): \(error)"
+
+        case .unknownError(let error):
+            "unknown error: \(error)"
+        }
+    }
+}
